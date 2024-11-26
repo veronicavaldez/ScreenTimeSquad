19ef3df3d24a92e35f7898b0e87c7378098da794
JavierM333 <11445jam@gmail.com>
2024-11-26 17:39:05 -0500

    Algorithms Setup
    - Adjusted User struct to hold user_id and duration
    - Added 5 new algorithm skeletons to organize functions we need to implement
    - cleaned out some un-necessary files

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -1,2 +1,26 @@
+#include "algorithms.h"
 
+void algorithms::printUserDuration(User &user) {
+    std::cout << "User ID: " << user.userID << " Duration: " << user.Duration << std::endl;
+}
+
+void algorithms::addDuration(User &user, double Duration) {
+    // add the duration to the User struct
+}
+
+double algorithms::durationCalc(int opened, int closed) {
+    // calculate the duration of the user
+}
+
+void algorithms::mergeSort() {
+    // figure out parameters needed
+}
+
+void algorithms::quickSort() {
+    // figure out parameters needed
+}
+
+void algorithms::Timer() {
+    // determine how long each algorithm takes to run
+}
 
